d824dbc07013675e7fa08ef16de40a3aa7c35c9f
Evgenii <111701135+eugenepokalyuk@users.noreply.github.com>
2024-08-12 17:31:45 +0700

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,30 +10,43 @@ const Header = () => (
 
         <nav>
           <ul className="flex flex-col md:flex-row items-center gap-[30px]">
-            <li><Link href="#" className="font-[400] px-[44px] py-[15px] text-[14px] leading-[22px] tracking-[0.5px] uppercase text-black hover:outline outline-black">Story</Link></li>
-            <li><Link href="#" className="font-[400] px-[44px] py-[15px] text-[14px] leading-[22px] tracking-[0.5px] uppercase text-black hover:outline outline-black">Shop</Link></li>
+            <li>
+              <Link href="#" className="font-[400] px-[44px] py-[15px] text-[14px] leading-[22px] tracking-[0.5px] uppercase text-black hover:outline outline-black">Story</Link>
+            </li>
+            
+            <li>
+              <Link href="#" className="font-[400] px-[44px] py-[15px] text-[14px] leading-[22px] tracking-[0.5px] uppercase text-black hover:outline outline-black">Shop</Link>
+            </li>
+            
             <li>
               <Link href="#" className="font-[400] px-[44px] py-[15px] text-[14px] leading-[22px] tracking-[0.5px] uppercase text-black flex items-center gap-[30px]">
                 <div className='w-[17px] h-[17px] border border-black rounded-full bg-[#FB4B4D]' />
+            
                 <Image
                   src="/images/logo.svg"
                   alt="Ujjo Logo"
                   width={61}
                   height={70}
                 />
+                
                 <div className='w-[17px] h-[17px] border border-black rounded-full bg-[#8B83F6]' />
               </Link>
             </li>
-            <li><Link href="#" className="font-[400] px-[44px] py-[15px] text-[14px] leading-[22px] tracking-[0.5px] uppercase text-black hover:outline outline-black">Contact</Link></li>
-            <li><Link href="#" className="font-[400] px-[44px] py-[15px] text-[14px] leading-[22px] tracking-[0.5px] uppercase text-black hover:outline outline-black">Wholesale</Link></li>
+            
+            <li>
+              <Link href="#" className="font-[400] px-[44px] py-[15px] text-[14px] leading-[22px] tracking-[0.5px] uppercase text-black hover:outline outline-black">Contact</Link>
+            </li>
+            
+            <li>
+              <Link href="#" className="font-[400] px-[44px] py-[15px] text-[14px] leading-[22px] tracking-[0.5px] uppercase text-black hover:outline outline-black">Wholesale</Link>
+            </li>
           </ul>
-
         </nav>
 
         <button className="font-[400] text-[14px] leading-[22px] tracing-[0.5px] uppercase px-[44px] py-[15px] bg-[#fb4b4d] text-white hover:brightness-90">Clone</button>
-
       </div>
     </div>
   </header>
 )
+
 export default Header;
