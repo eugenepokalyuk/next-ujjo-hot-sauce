46e94ae1bd986a6754b23096e4a2115d4da12285
Evgenii <eugene.pokalyuk@gmail.com>
2025-06-14 02:30:25 +0700

    refs 1 fix links

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,35 +8,35 @@ import Marquee from '@/components/Marquee';
 import Products from '@/components/Products';
 import Story from '@/components/Story';
 
-const Home = () => {
-    return (
-        <div className='md:w-[1440px] mx-auto p-5'>
-            <Head>
-                <title>Ujjo - Hot Sauce for Coffee</title>
-                <meta charSet="UTF-8" />
-                <meta name="description" content="The first hot sauce made for coffee." />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <link rel="icon" href="/favicon.svg" />
-            </Head>
+const Home = () => (
+    <div className='md:w-[1440px] mx-auto p-5'>
+        <Head>
+            <meta charSet="UTF-8"/>
+            <meta name="description" content="The first hot sauce made for coffee."/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <link rel="icon" href="/favicon.svg"/>
 
-            <Header />
-            <main>
-                <Hero />
-                
-                <Products />
-                
-                <Story />
-                
-                <div className='border-x border-black'>
-                    <Marquee />
-                </div>
-                
-                <Banner />
-            </main>
-            
-            <Footer />
-        </div>
-    );
-};
+            <title>Ujjo - Hot Sauce for Coffee</title>
+        </Head>
+
+        <Header/>
+        
+        <main>
+            <Hero/>
+
+            <Products/>
+
+            <Story/>
+
+            <div className='border-x border-black'>
+                <Marquee/>
+            </div>
+
+            <Banner/>
+        </main>
+
+        <Footer/>
+    </div>
+);
 
 export default Home;
